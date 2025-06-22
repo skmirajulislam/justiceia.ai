d5a4dd6146cebd5f7f892eb93d3e97f06c6602c8
skmirajulislam <skmirajulislam181@gmail.com>
2025-06-22 20:10:18 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
   images: {
     domains: ['your-image-domain.com'], // Add your image domains here if needed
   },
