938f91eeb4c2e8b1cb3ebce1a62189d50f180266
Bhumi Prasad <prasad14bhumi@gmail.com>
2025-06-23 19:53:35 +0530

    vkyc and legal library ui update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['your-image-domain.com'], // Add your image domains here if needed
+    domains: ['images.unsplash.com'], // Add your image domains here if needed
   },
 };
 
